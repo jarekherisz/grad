aaa862fb2f59209eaae6e84abb87e5e78fc91dae
Jaroslaw Herisz <jarek@herisz.pl>
2022-01-13 17:53:28 +0100

    Create User.php

---
+++ lib/CoreBundle/src/Entity/User.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Grad\CoreBundle\Entity;
+
+use Grad\CoreBundle\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 32)]
+    private $first_name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->first_name;
+    }
+
+    public function setFirstName(string $first_name): self
+    {
+        $this->first_name = $first_name;
+
+        return $this;
+    }
+}
