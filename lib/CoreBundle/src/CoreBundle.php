6ca32a06faae6b1a4d5dee30a1bf69a7a905fce2
Jaroslaw Herisz <jarek@herisz.pl>
2022-01-17 19:37:19 +0100

    Prepare grad command console

--- lib/CoreBundle/src/CoreBundle.php
+++ lib/CoreBundle/src/CoreBundle.php
@@ -2,9 +2,28 @@
 
 namespace Grad\CoreBundle;
 
+use Grad\CoreBundle\DependencyInjection\AddConsoleCommandPass;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\Config\Resource\ClassExistenceResource;
 
 class CoreBundle extends Bundle
 {
 
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $this->addCompilerPassIfExists($container, AddConsoleCommandPass::class, PassConfig::TYPE_BEFORE_REMOVING);
+    }
+
+    private function addCompilerPassIfExists(ContainerBuilder $container, string $class, string $type = PassConfig::TYPE_BEFORE_OPTIMIZATION, int $priority = 0)
+    {
+        $container->addResource(new ClassExistenceResource($class));
+
+        if (class_exists($class)) {
+            $container->addCompilerPass(new $class(), $type, $priority);
+        }
+    }
 }
