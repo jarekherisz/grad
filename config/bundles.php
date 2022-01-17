5e9ff4216efac9cfb54f20d3794deb71f9a5e017
Jaroslaw Herisz <jarek@herisz.pl>
2022-01-17 19:38:19 +0100

    remove doctrine/doctrine-migrations-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -3,6 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    \Grad\CoreBundle\CoreBundle::class => ['all' => true],
+    Grad\CoreBundle\CoreBundle::class => ['all' => true],
 ];
