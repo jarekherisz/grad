b216f10f6cd8cba063329abf28a9dd0cc1dfe1b8
Jaroslaw Herisz <jarek@herisz.pl>
2022-01-18 17:27:52 +0100

    Update Kernel.php

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,9 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public const GRAD_VERSION = '0.0.1';
+    public const GRAD_MAJOR_VERSION = 0;
+    public const GRAD_MINOR_VERSION = 0;
+    public const GRAD_RELEASE_VERSION = 1;
 }
