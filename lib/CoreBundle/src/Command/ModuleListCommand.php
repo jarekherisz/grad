cd92c5a0b617b6daca7183ba57faabe133f93bf4
Jaroslaw Herisz <jarek@herisz.pl>
2022-01-19 18:19:10 +0100

    Support for the installed module version

--- lib/CoreBundle/src/Command/ModuleListCommand.php
+++ lib/CoreBundle/src/Command/ModuleListCommand.php
@@ -2,6 +2,8 @@
 
 namespace Grad\CoreBundle\Command;
 
+use Grad\CoreBundle\Entity\ModuleVersion;
+use Grad\CoreBundle\Repository\ModuleVersionRepository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
@@ -11,10 +13,15 @@ use Symfony\Component\Console\Style\StyleInterface;
 
 class ModuleListCommand extends Command
 {
-    public function __construct(string $name = null)
+    protected ModuleVersionRepository $moduleVersionRepository;
+
+    public function __construct(ModuleVersionRepository $moduleVersionRepository, string $name = null)
     {
+        $this->moduleVersionRepository = $moduleVersionRepository;
+
         $this->setDescription("List of modules");
         parent::__construct("module:list");
+
 
     }
 
@@ -24,16 +31,21 @@ class ModuleListCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $title = 'Grad modules list';
-        $headers = ['Bundle name', 'Extension alias', 'Class', 'Version'];
+        $headers = ['Bundle name', 'Extension alias', 'Class', 'Version', 'Installed version'];
         $rows = [];
+
 
         $bundles = $this->getApplication()->getKernel()->getBundles();
 
         foreach ($bundles as $bundle) {
-            if($bundle instanceof GradBundle)
-            {
+            $moduleVersion = $this->moduleVersionRepository->findOneByClass(get_class($bundle));
+            $installedVersion = "Not installed";
+            if ($moduleVersion != null)
+                $installedVersion = $moduleVersion->getVersion();
+
+            if ($bundle instanceof GradBundle) {
                 $extension = $bundle->getContainerExtension();
-                $rows[] = [$bundle->getName(), $extension ? $extension->getAlias() : '', get_class($bundle), $bundle->getVersion()];
+                $rows[] = [$bundle->getName(), $extension ? $extension->getAlias() : '', get_class($bundle), $bundle->getVersion(), $installedVersion];
             }
         }
 
